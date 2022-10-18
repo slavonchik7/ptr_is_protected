1efa479eb90612e809acad9a25679487a4115363
slavonchik7 <slavonchik0701@gmail.com>
2022-10-18 23:23:43 +0300

    added defines for checking of few errors

---
+++ errcheck.h
@@ -0,0 +1,52 @@
+
+
+#ifndef ERRCHECK_H
+#define ERRCHECK_H
+
+
+#define __TRACK_CHECK_RET_INIT_REQ(ret) \
+    do { \
+        if ( !main_track_list ) { \
+            errtrack = ETRACK_INIT_REQUIRED; \
+            return ret; \
+        } \
+    } while ( 0 )
+
+
+#define __TRACK_CHECK_RET_NULL_PTR(p, ret) \
+    do { \
+        if ( !main_track_list ) { \
+            errtrack = ETRACK_NULL_PTR_PASSED; \
+            return ret; \
+        } \
+    } while ( 0 )
+
+
+#define __TRACK_CHECK_RET_MEM_CHANGED(pscore, ret) \
+    do { \
+        if ( __track_memory_changed(pscore) ) { \
+            errtrack = ETRACK_MEM_WAS_CHANGED; \
+            return ret; \
+        } \
+    } while ( 0 )
+
+
+#define __TRACK_CHECK_WENT_UPPER_LIM(pscore, ret) \
+    do { \
+        if ( pscore->mem_cur_addr > pscore->mem_end_addr ) { \
+            errtrack = ETRACK_MEM_WAS_CHANGED; \
+            return ret; \
+        } \
+    } while ( 0 )
+
+
+#define __TRACK_CHECK_WENT_LOWER_LIM(pscore, ret) \
+    do { \
+        if ( pscore->mem_cur_addr < pscore->mem_start_addr ) { \
+            errtrack = ETRACK_MEM_WAS_CHANGED; \
+            return ret; \
+        } \
+    } while ( 0 )
+
+
+#endif /* ERRCHECK_H */
