4ae607c8333c3603de9cce5a4645109db959afe5
slavonchik7 <slavonchik0701@gmail.com>
2022-10-20 00:23:22 +0300

    modified

--- crc32.h
+++ crc32.h
@@ -95,7 +95,7 @@ uint_least32_t Crc32(const unsigned char * buf, size_t len)
 {
     uint_least32_t crc = 0xFFFFFFFF;
     while (len--)
-        crc = (crc >> 8) ^ Crc32Table[(crc ^ *buf++) & 0xFF];
+        crc = (crc >> 8) ^ Crc32Table[(crc ^ (*buf++)) & 0xFF];
     return crc ^ 0xFFFFFFFF;
 }
 
